40567a249141f2efb4e4c79c870eab1abf3f6ce3
gabrielemariotti <gabri.mariotti@gmail.com>
2013-09-02 23:27:52 +0200

    Added About Menu in demo app

--- ColorPicker/src/it/gmariotti/android/example/colorpicker/Utils.java
+++ ColorPicker/src/it/gmariotti/android/example/colorpicker/Utils.java
@@ -15,9 +15,25 @@
  *******************************************************************************/
 package it.gmariotti.android.example.colorpicker;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 /**
  * 
@@ -70,5 +86,66 @@ public class Utils {
         return (context.getResources().getConfiguration().screenLayout
                 & Configuration.SCREENLAYOUT_SIZE_MASK)
                 >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+	
+	public static void showAbout(Activity activity) {
+
+        FragmentManager fm = activity.getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment prev = fm.findFragmentByTag("dialog_about");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        new AboutDialog().show(ft,"dialog_about");
+    }
+
+    /**
+     * About Dialog
+     */
+    public static class AboutDialog extends DialogFragment {
+
+        private static final String VERSION_UNAVAILABLE = "N/A";
+
+        public AboutDialog() {
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get app version
+            PackageManager pm = getActivity().getPackageManager();
+            String packageName = getActivity().getPackageName();
+            String versionName;
+            try {
+                PackageInfo info = pm.getPackageInfo(packageName, 0);
+                versionName = info.versionName;
+            } catch (PackageManager.NameNotFoundException e) {
+                versionName = VERSION_UNAVAILABLE;
+            }
+
+            // Build the about body view and append the link to see OSS licenses
+            SpannableStringBuilder aboutBody = new SpannableStringBuilder();
+            aboutBody.append(Html.fromHtml(getString(R.string.about_body, versionName)));
+
+
+            LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            TextView aboutBodyView = (TextView) layoutInflater.inflate(R.layout.dialog_about, null);
+            aboutBodyView.setText(aboutBody);
+            aboutBodyView.setMovementMethod(new LinkMovementMethod());
+
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.title_about)
+                    .setView(aboutBodyView)
+                    .setPositiveButton(R.string.about_ok,
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    dialog.dismiss();
+                                }
+                            }
+                    )
+                    .create();
+        }
     }
 }
