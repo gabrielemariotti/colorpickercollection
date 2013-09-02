b2dec1a6543e565784930398ea3377eb14947d2d
gabrielemariotti <gabri.mariotti@gmail.com>
2013-09-02 23:26:55 +0200

    Added ColorPickerPreference based on StockCalendar ColorPicker

---
+++ ColorPicker/src/it/gmariotti/android/example/colorpicker/calendarstock/SettingsPickerActivity.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright 2013 Gabriele Mariotti
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package it.gmariotti.android.example.colorpicker.calendarstock;
+
+import it.gmariotti.android.example.colorpicker.R;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+public class SettingsPickerActivity extends PreferenceActivity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// In the simplified UI, fragments are not used at all and we instead
+        // use the older PreferenceActivity APIs.
+
+        // Add 'general' preferences.
+        addPreferencesFromResource(R.xml.pref_calendarcolor);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+}
